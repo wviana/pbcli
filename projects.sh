b6eac392b4fc8882426c4b4a7d6d2257628a7cd0
Wesley Viana <viana.wesley@gmail.com>
2018-02-14 23:36:01 -0200

    Removed wrong placed space

--- projects.sh
+++ projects.sh
@@ -4,7 +4,7 @@ function project-list() {
     curl 'https://projectbox.softbox.com.br/api/projetos' \
     -XPOST \
     -H 'Content-Type: application/json' \
-    -H 'Accept: app lication/json, text/plain, */*' \
+    -H 'Accept: application/json, text/plain, */*' \
     -H "$(cat cookie)" \
     --data-binary '{"query":{"nao_buscar_info_detalhada":true},"opts":{"sort":{"key":"nome","order":1}},"cols":null,"id_usuario":196}' 2> /dev/null | \
     jq  --raw-output ' .data | .[] | [.id_unidade, .nome, .pontos_totais, .pontos_previstos, .sprints_previstos, .pontos_concluidos, .dt_inicio, .num_tarefas_criadas, .status ] | @tsv'
