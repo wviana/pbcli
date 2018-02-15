8125f91179d1534343176ef507fd8ccc3475a5a2
Wesley Viana <viana.wesley@gmail.com>
2018-02-14 23:06:25 -0200

    Removed unecessary code

--- projects.sh
+++ projects.sh
@@ -5,14 +5,6 @@ source http.sh
 function project-list() {
     http post '/projetos' '{"query":{"nao_buscar_info_detalhada":true},"opts":{"sort":{"key":"nome","order":1}},"cols":null,"id_usuario":196}' \
     | jq  '  .data | .[] | .nome '
-
-    # CODIGO ANTIGO DO CURL
-    # curl 'https://projectbox.softbox.com.br/api/projetos' \
-    # -XPOST \
-    # -H 'Content-Type: application/json' \
-    # -H 'Accept: application/json, text/plain, */*' \
-    # -H "$(cat cookie)" \
-    # --data-binary '{"query":{"nao_buscar_info_detalhada":true},"opts":{"sort":{"key":"nome","order":1}},"cols":null,"id_usuario":196}' 2> /dev/null | jq  '  .data | .[] | .nome '
 }
 
 function project() {
